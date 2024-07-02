a191c7a3b72b13fb8f871d2692678176ba2b453d
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-02 16:40:42 +0900

    Badge-gsap(Animation)

--- js/main.js
+++ js/main.js
@@ -23,11 +23,18 @@ window.addEventListener(
     console.log(window.scrollY);
     if (window.scrollY > 500) {
       //Badge 숨기기
-      badgeEl.style.display = "none";
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
     } else {
       //Badge 나타내기
-      badgeEl.style.display = "block";
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
     }
   }, 300)
 );
 //_.throttle( 함수, 시간 (ms))
+// gsap.to(요소, 지속시간, 옵션)
