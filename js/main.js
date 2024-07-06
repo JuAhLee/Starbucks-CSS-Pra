bb1d92ab20598f86d7bd06d6e10b0b41f3c352d1
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-06 18:56:24 +0900

    YOUTUBE Section(BackgroundImg)

--- js/main.js
+++ js/main.js
@@ -87,3 +87,22 @@ promotionToggleBtn.addEventListener("click", function () {
     promotionEl.classList.remove("hide");
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut, // ease 함수
+    delay: random(0, delay),
+  });
+}
+
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating1", 0.5, 15);
+floatingObject(".floating1", 1.5, 20);
